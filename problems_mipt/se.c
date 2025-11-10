168b6d449e0656ece1417ec49da17c151e144e48
sanueee <109550742+sanueee@users.noreply.github.com>
2025-11-11 00:41:43 +0300

    solved pr_se

--- problems_mipt/se.c
+++ problems_mipt/se.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct sieve_t {
         unsigned size;
@@ -15,48 +16,39 @@ void free_sieve(struct sieve_t* s);
 int main() {
     int n;
     scanf("%d", &n);
-    
     struct sieve_t s;
     s.sieve = calloc((n - 1), sizeof(char));
     s.size = n;
     init_sieve(n, &s);
     printf("%d", count_primes(n, &s));
+    free_sieve(&s);
     return 0;
 }
 
 void init_sieve(int n, struct sieve_t* s) {
-    int k = 0;
+    /*
     for (int i = 0; i < (n - 1); i++) {
         assert((s->sieve + i) != NULL);
         *(s->sieve + i) = i + 2;
     }
-    /*
     print_sieve(s, n);
     printf("\n");
-    */
-    for (int i = 0; i < (n - 1); i++) {
-        int k = 0, j = 0;
-        if (s->sieve[i] == 0) { // если уже побывали на 4 во время того как i = 2 например
-            continue;
-        }
-        k = s->sieve[i]; // шаг
-        j = i; // шаг внутри шага 
-        while (j < n) {
-            if (s->sieve[j] == k) {
+    */    
+    s->sieve[0] = s->sieve[1] = 1;  // не простые
+    int sqrt_n = (int)sqrt(n);
+    for (int i = 2; i <= (sqrt_n + 1); i++) {
+        if (!s->sieve[i]) {
+            for (int j = i * i; j <= n + 1; j += i) {  // начинаем с i*i, т.к. меньшие кратные уже вычеркнуты
                 s->sieve[j] = 1;
-                j+=k;
-                continue;
             }
-            s->sieve[j] = 0;
-            j+=k;
         }
     }
 }
 
 int count_primes(int n, struct sieve_t* s) {
     int counter = 0;
-    for (int i = 0; i < n-1; i++) {
-        if (s->sieve[i] == 1) {
+    for (int i = 0; i < n + 1; i++) {
+        if (s->sieve[i] == 0) {
             counter++;
         }
     }
