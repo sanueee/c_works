f50ec817f5bb6d964e9aba151dcc5d0a0cdf3106
sanueee <109550742+sanueee@users.noreply.github.com>
2025-11-11 15:00:27 +0300

    solved pr_rl

--- problems_mipt/rl.c
+++ problems_mipt/rl.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
-unsigned long long gcd(unsigned long long x, unsigned long long y);
+long long gcd_r(long long x, long long y);
+long long gcd_c(long long x, long long y);
 
-int main() {
-	unsigned long long x = 0, y = 0, g;
-	int res;
-	res = scanf("%llu %llu", &x, &y);
-	//assert(res==2);
-	g = gcd(x, y);
-	printf("%llu\n", g);
+int main(void) {
+	long long x = 0, y = 0;
+	scanf("%lld %lld", &x, &y);
+
+	long long g = gcd_c(x, y);
+	printf("%lld\n", g);
+
 	return 0;
 }
 
-unsigned long long gcd(unsigned long long x, unsigned long long y) {
+long long gcd_r(long long x, long long y) {
+    if (y > x) {
+        return gcd_r(y, x); // x > y
+    }
+    x = llabs(x);
+    y = llabs(y);
+    int q = x % y;
+    if (q == 0) {
+        return y; // если делятся без остатка возвращаем меньшее
+    }
+    return gcd_r(y, q); // gcd(a, b) == gcd(b, q)
+}
 
-    if (x > y) {
-    unsigned long long temp = x;
-    x = y;
-    y = temp;
+long long gcd_c(long long x, long long y) {
+    if (y > x) {
+        return gcd_c(y, x); // x > y
     }
-    // x < y - гарантируем 
-    unsigned long long r = y % x; // остаток
-    unsigned long long q = x; // то че делим
-    while (1) {
-        if (r == 0) return q;
-        r = q % r;
-        q = r;   
+    x = llabs(x);
+    y = llabs(y);
+    while (y != 0) {
+        long long temp = y;
+        y = x % y;
+        x = temp;
     }
+    return x;
 }
